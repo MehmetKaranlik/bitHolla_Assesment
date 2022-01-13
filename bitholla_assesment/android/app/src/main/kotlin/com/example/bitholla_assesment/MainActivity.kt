f7450f14f1733e3ab0fa820d003ecc5fa1bd5aa1
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-01-13 21:41:22 +0300

    ASDSA

---
+++ bitholla_assesment/android/app/src/main/kotlin/com/example/bitholla_assesment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bitholla_assesment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bitholla_assesment/lib/view/coin_detail/view/coin_detail_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CoinDetailView extends StatelessWidget {
+  const CoinDetailView({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
